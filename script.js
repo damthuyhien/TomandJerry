6c60125d0524edbf41e23f0b91d678242593c5df
hoanhkhoa <anhkhoa12204@gmail.com>
2025-04-08 08:45:50 +0700

    add js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Lấy nút từ DOM
+const actionButton = document.getElementById('actionButton');
+
+// Thêm sự kiện click vào nút
+actionButton.addEventListener('click', function() {
+    // Hiển thị thông báo với tên các thành viên nhóm
+    alert("Tom: thuyhien, Jerry: baongoc, Spike: anhkhoa");
+});
